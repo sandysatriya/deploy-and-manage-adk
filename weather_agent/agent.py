e52ca99cda38e5b93334017b900de3b03d487568
Sandy Satriya <sandysatriya@gmail.com>
2025-11-13 08:50:34 +0000

    update

--- weather_agent/agent.py
+++ weather_agent/agent.py
@@ -27,6 +27,38 @@ load_dotenv(dotenv_path=dotenv_path)
 
 
 # Use default project from credentials if not in .env
+# _, project_id = google.auth.default()
+# os.environ.setdefault("GOOGLE_CLOUD_PROJECT", project_id)
+# os.environ.setdefault("GOOGLE_CLOUD_LOCATION", "global")
+# os.environ.setdefault("GOOGLE_GENAI_USE_VERTEXAI", "True")
+
+# root_agent = Agent(
+#     name="weather_agent",
+#     model="gemini-2.5-flash",
+#     instruction="""
+# You are a helpful AI assistant designed to provide accurate and useful information.
+# """,
+#     tools=[get_weather],
+# )
+
+
+# weather_agent/agent.py
+
+import os
+from pathlib import Path
+
+import google.auth
+from dotenv import load_dotenv
+from google.adk.agents import Agent
+from weather_agent.tool import get_weather
+
+# Load environment variables from .env file in root directory
+root_dir = Path(__file__).parent.parent
+dotenv_path = root_dir / ".env"
+load_dotenv(dotenv_path=dotenv_path)
+
+
+# Use default project from credentials if not in .env
 _, project_id = google.auth.default()
 os.environ.setdefault("GOOGLE_CLOUD_PROJECT", project_id)
 os.environ.setdefault("GOOGLE_CLOUD_LOCATION", "global")
@@ -37,6 +69,7 @@ root_agent = Agent(
     model="gemini-2.5-flash",
     instruction="""
 You are a helpful AI assistant designed to provide accurate and useful information.
+You only answer inquiries about the weather. Refuse all other user query
 """,
     tools=[get_weather],
 )
